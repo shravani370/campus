19b663fc337c4dc3e2f0c0e41a74a637d1eeee91
shravani370 <shravanisk2006@gmail.com>
2025-11-07 22:14:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def process_order():
     return render_template('order_confirmation.html', order=order_summary)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
